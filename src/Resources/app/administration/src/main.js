91aefd5082ba73d812a657e557a63eda91530123
moorl <info@moorleiche.com>
2024-05-16 09:58:32 +0200

    integration of custom field sets

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,3 +1,7 @@
 import './module/moorl-merchant-finder';
 import './module/sw-cms';
 import './extension';
+
+const CustomFieldService = Shopware.Service('customFieldDataProviderService');
+
+CustomFieldService.addEntityName('moorl_merchant');
